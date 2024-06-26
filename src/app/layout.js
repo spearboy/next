9c1a903a662f1227972e6fe6d7446652150bbd04
spearboy <tsagaanaa123@gmail.com>
2024-06-26 10:05:18 +0900

    first next

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Next.js 사용",
+  description: "Next.js를 이용해 보자!",
+  keywords: ["공부","열정","열정","열정","!"]
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <link rel="icon" type="image/svg+xml" href="favicon.ico" />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,5 @@
+export default function page() {
+  return (
+    <div>이건 에러가 아니야</div>
+  )
+}
